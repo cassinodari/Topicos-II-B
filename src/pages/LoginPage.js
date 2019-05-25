49b627180995c80d6412e0ef4de044c930a58093
cassinodari <cassi.nodari@hotmail.com>
2019-05-24 21:33:40 -0300

    first commit

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class LoginPage extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>Login Page!</Text>
+            </View >
+        );
+    }
+};
